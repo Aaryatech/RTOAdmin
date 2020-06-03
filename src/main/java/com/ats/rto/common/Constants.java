5e93b7a42de65f22af867350f358cdb04b685f22
maddy <maddy@maddy-Inspiron-3541>
2020-06-03 16:17:05 +0530

    //maddy

--- src/main/java/com/ats/rto/common/Constants.java
+++ src/main/java/com/ats/rto/common/Constants.java
@@ -1,9 +1,11 @@
 package com.ats.rto.common;
 
 public class Constants {
-	public static final String url = "http://tomcat.aaryatechindia.in:2908/rtowebapi/";
+	//public static final String url="http://localhost:8093/";
+	public static final String url="http://107.180.91.43:8080/rtowebapi/";
 	public static final int CAT_FILE_TYPE = 0;
-	public static final String IMAGE_FOLDER = "/home/lenovo/uploads/";
-	public static final String RTO_DOC_URL = "http://tomcat.aaryatechindia.in:2908/rtodocupload/";
+	public static final String IMAGE_FOLDER = "http://107.180.91.43:8080/rto_uploads/rtodocupload/";
+	public static final String RTO_DOC_URL = "http://107.180.91.43:8080/rto_uploads/rtodocupload/";
+	public static String REPORT_SAVE="/opt/apache-tomcat-8.5.49/webapps/rto_uploads/Reports.pdf";
 
 }
